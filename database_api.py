625db5b108e007655b014d1558615f1d1d401547
Russell Cullen <russellscullen@gmail.com>
2012-10-18 00:18:14 -0400

    Fixed some function names and updated documentation.

--- database_api.py
+++ database_api.py
@@ -1,12 +1,12 @@
 ## Updating the global ingredients and recipes database
 
 # input  : ingredient object
-def insert_ingredient_all(ingredient):
-  # creates base ingredient and places in Ingredients collection
+def add_ingredient(ingredient):
+  # Adds ingredient to Ingredients collection
 
 # input  : recipe object
-def insert_recipe_all(recipe):
-  # creates base recipe and places in Recipes collection
+def add_recipe(recipe):
+  # Adds recipe to Recipes collection
 
 
 # input  : name of ingredient (as string)
@@ -38,10 +38,6 @@ def get_current_ingredients(fridge):
 
 # input  : ingredient name, fridge name
 def insert_ingredient(ingredient, fridge):
-  # create fridge ingredient JSON object
-  # insert into ingredients array of fridge
-  # look in partial_recipes array of fridge, update ingredients_needed of each partial recipe
-  # if partial_recipe's ingredients_needed is empty, place recipe in current_recipes, remove from partial_recipes
 
 # input  : ingredient name, quantity, fridge name
 def update_ingredient(ingredient, quantity, fridge):
@@ -53,19 +49,3 @@ def can_cook(recipe, fridge):
   # look through Recipe Ingredient objects, compare to Fridge Ingredients
   # if all Recipe Ingredients are accounted for, return true
   # else, return false
-
-# input  : recipe name as string
-# return : recipe to be displayed as JSON object
-def display_recipe(recipe):
-  # get recipe object
-  # convert into JSON
-
-
-
-
-
-
-
-
-
-
