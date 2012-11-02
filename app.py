22a3c573e85a65da8c471d134644e1434cd092d7
Russell Cullen <russellscullen@gmail.com>
2012-11-02 00:09:30 -0400

    Heroku support

---
+++ app.py
@@ -0,0 +1,16 @@
+import tornado.ioloop
+import tornado.web
+import os
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    application.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
