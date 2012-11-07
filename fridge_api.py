f6125aecdb772338f2b89e455a480a0a8a533133
Russell Cullen <russellscullen@gmail.com>
2012-11-07 16:59:30 -0500

    Removed duplicate function

--- fridge_api.py
+++ fridge_api.py
@@ -28,10 +28,6 @@ class FridgeApi:
 
 		return hasIngredients
 
-	def search_recipes(self, query):
-		tag_list = str.split(query)
-		return self.api.find_recipe_by_tag(tag_list)
-
 	def search_fridge_recipes(self, query, fridge):
 		recipe_list = self.search_recipes(query)
 		for r in recipe_list:
