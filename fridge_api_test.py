ea0126b5aa9ebb6b351e03b6cfc5a00fce425765
Jonathan Hsu <jreucypoda@gmail.com>
2012-11-14 22:54:54 -0500

    added fridge_api_test skeleton, removed extraneous files

---
+++ fridge_api_test.py
@@ -0,0 +1,26 @@
+import unittest
+import api.database_api
+import api.fridge_api
+import api.creator
+import fake_data
+
+class fridge_api_test(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_can_cook(self):
+		pass
+
+	def test_search_fridge_recipes(self):
+		pass
+
+	def test_suggest_by_current_recipe(self):
+		pass
+
+	def test_add_item_to_grocery_list(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
