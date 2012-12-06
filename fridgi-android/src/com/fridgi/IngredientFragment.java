0259dfab050066d59f6ae0cb5fb51f9664d14e51
Russell Cullen <russellscullen@gmail.com>
2012-12-05 19:35:07 -0500

    Added ability to remove from grocery list

--- fridgi-android/src/com/fridgi/IngredientFragment.java
+++ fridgi-android/src/com/fridgi/IngredientFragment.java
@@ -1,14 +1,20 @@
 package com.fridgi;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.fridgi.adapters.IngredientAdapter;
+import com.fridgi.models.RecipeIngredient;
 import com.fridgi.tasks.FridgeTask;
 import com.fridgi.tasks.FridgeTask.FridgeCallback;
+import com.fridgi.tasks.RemoveFromGroceryTask;
 import com.fridgi.util.Globals;
 
 public class IngredientFragment extends ListFragment implements FridgeCallback {
@@ -20,6 +26,7 @@ public class IngredientFragment extends ListFragment implements FridgeCallback {
     
     int mType;
     IngredientAdapter mAdapter;
+    private RecipeIngredient mSelected;
 
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -27,6 +34,7 @@ public class IngredientFragment extends ListFragment implements FridgeCallback {
         if (mType == TYPE_GROCERY) {
             mAdapter = new IngredientAdapter(getActivity(), Globals.getInstance().getFridge().getGroceryList());
             getListView().setAdapter(mAdapter);
+            getListView().setOnItemClickListener(mOnIngredientClickedListener);
             FridgeTask task = new FridgeTask(Globals.getInstance().getFridge().getName(), this);
             task.execute();
         } else if (mType == TYPE_FRIDGE) {
@@ -44,10 +52,42 @@ public class IngredientFragment extends ListFragment implements FridgeCallback {
             mAdapter.setIngredients(Globals.getInstance().getFridge().getIngredients());
         }
     }
+    
+    public void refresh() {
+        FridgeTask fridge = new FridgeTask(Globals.getInstance().getFridge().getName(), IngredientFragment.this);
+        fridge.execute();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ingredient_detail, container, false);
     }
+    
+    OnItemClickListener mOnIngredientClickedListener = new OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Object obj = mAdapter.getItem(position);
+            if (obj instanceof RecipeIngredient) {
+                mSelected = (RecipeIngredient) obj;
+                AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                    .setMessage("Are you sure you want to remove " + mSelected.getName() + " from your list?")
+                    .setPositiveButton("OK", mOnRemoveListener)
+                    .create();
+                dialog.show();
+            }
+        }
+    };
+    
+    DialogInterface.OnClickListener mOnRemoveListener = new DialogInterface.OnClickListener() {
+        
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            RemoveFromGroceryTask task = new RemoveFromGroceryTask(Globals.getInstance().getFridge().getName(), 
+                    mSelected.getIngredient().getId());
+            task.execute();
+            refresh();
+        }
+    };
 }
